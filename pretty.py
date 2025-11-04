5af5178f96da5062059307ca162da5cfe5bc67a1
star-dev-real <email170512@gmail.com>
2025-11-04 21:54:13 +0000

    Initial commit

--- pretty.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open("JSON/content.json", "r", encoding="utf-8") as f:
-    data = json.load(f)
-
-with open("JSON/content.json", "w", encoding="utf-8") as f:
-    json.dump(data, f, indent=2)
-
-print("JSON file formatted successfully!")
